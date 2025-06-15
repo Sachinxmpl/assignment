066b4f98b0b80852000525a8c726b0f956e7c703
Sachinxmpl <sachinxmpl6@gmail.com>
2025-06-15 11:02:31 +0545

    add cors index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,17 +7,25 @@ import { errorMiddleware } from "./middlewares/error";
 
 import authRoutes from './routes/authRoutes'
 import bookRoutes from './routes/bookRoutes'
+import borrowRoutes from './routes/borrowRoutes'
+import categoryRoutes from './routes/categoryRoutes'
+import reviewRoutes from './routes/reviewRoutes'
+import userRoutes from './routes/userRoutes'
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json())
 app.use(passport.initialize())
 
 app.use('/auth' , authRoutes)
 app.use('/books' , bookRoutes)
-
+app.use('/borrows', borrowRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/reviews', reviewRoutes);
+app.use('/users', userRoutes);
 
 app.use(errorMiddleware);
 
